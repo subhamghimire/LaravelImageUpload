0ce521ebd6fb968fad045e635ac0fa48254a9683
subhamghimire <subham.ghimire26@gmail.com>
2020-02-02 09:33:47 +0545

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layout')
+@section('content')
+    <div class="container">
+    <form action="{{ route('file.store') }}" method="post" enctype="multipart/form-data">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <div class="card-title ">
+                            <h4> Laravel Upload Image </h4>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+
+                    <!-- print success message after file upload  -->
+                            @if(Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{ Session::get('success') }}
+                                    @php
+                                        Session::forget('success');
+                                    @endphp
+                                </div>
+                            @endif
+
+                            <div class="form-group" {{ $errors->has('filename') ? 'has-error' : '' }}>
+                                <label for="filename"></label>
+                                    <input type="file" name="filename" id="filename" class="form-control-file">
+                                    <span class="text-danger"> {{ $errors->first('filename') }}</span>
+                            </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-md"> Upload </button>
+                        </div>
+                        {{ csrf_field() }}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+@section('content')
+<div class="container mt-5 border p-3">
+    <div class="row pb-5">
+        <div class="col-xl-12">
+            <a href=" {{ route('file.create') }} " class="btn btn-success btn-sm float-right"> Add Image </a>
+        </div>
+    </div>
+
+    <div class="row">
+        @foreach($files as $file)
+        <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6 m-auto p-2">
+            <div class="card shadow">
+                <div class="card-body">
+                    <img src="storage/{{ $file->filename }} " class="img-fluid img-thumbnails">
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+    @endsection
